6a29a0dd528996282c645a01673d0c5e75ed2079
savajeremic96 <sava.jeremic@insidemaps.com>
2025-05-26 15:24:23 +0200

    on password update, remove account lock and failed login count fields

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -205,7 +205,10 @@ export class UserController extends AdaptableController {
       // clear reset password token
       .then(() => this.config.database.update('_User', {username}, {
         _perishable_token: {__op: 'Delete'},
-        _perishable_token_expires_at: {__op: 'Delete'}
+        _perishable_token_expires_at: {__op: 'Delete'},
+        // On successful password reset, we also clear the failed login count and lockout expiration.
+        _failed_login_count: {__op: 'Delete'},
+        _account_lockout_expires_at: {__op: 'Delete'},
       })).catch((error) => {
         if (error.message) {  // in case of Parse.Error, fail with the error message only
           return Promise.reject(error.message);
